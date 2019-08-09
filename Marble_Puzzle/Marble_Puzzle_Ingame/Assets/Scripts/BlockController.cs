4198e0ca253213323bfd9f8c8efe8acc59654297
Moon <mbs95@naver.com>
2019-08-09 20:29:45 +0900

    Own game Ingame
    판 구현 완료

---
+++ Marble_Puzzle/Marble_Puzzle_Ingame/Assets/Scripts/BlockController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockController : MonoBehaviour
+{
+    public int myType;
+    private GameObject blocks;
+    private Vector2 originPos;
+
+    private void Start()
+    {
+        blocks = transform.parent.gameObject;
+    }
+
+    private void OnMouseDown()
+    {
+        originPos = blocks.transform.position;
+        BoardManager.instance.holdingBlock = blocks;
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        blocks.transform.position = mousePos;
+    }
+
+    private void OnMouseUp()
+    {
+        if (BoardManager.instance.CheckFit() == false)
+        {
+            BackToOriginPos();
+        }
+        else
+        {
+            BoardManager.instance.FitBlocks();
+        }
+    }
+
+    private void BackToOriginPos()
+    {
+        blocks.transform.position = originPos;
+        BoardManager.instance.holdingBlock = null;
+    }
+}
